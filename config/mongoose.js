0d91847fa3949108d399a2d2ae8af94ea219f102
spankajchauhan <96768418+spankajchauhan@users.noreply.github.com>
2022-11-08 09:59:44 +0530

    ADD new

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(`mongodb+srv://spankaj0592:809080@cluster0.biakhid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+
+// ("mongodb://localhost/placement_DB");
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Error Connecting to MongoDB"));
+
+db.once("open", function () {
+  console.log("Connected to DB");
+});
+
+module.exports = db;
